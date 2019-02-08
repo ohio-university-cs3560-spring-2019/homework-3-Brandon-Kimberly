4768bf349c5e220d0cc032589b9244061664f61b
Brandon Kimberly <bk685616@ohio.edu>
2019-02-07 19:58:59 -0500

    Changed obvious errors

--- hw_broken.cc
+++ hw_broken.cc
@@ -16,7 +16,7 @@ int main()
 	}
 }
 
-int deviation( int* a, int n )
+double deviation( int* a, int n )
 {
 	int sum;
 	for(size_t i = 0; i <= v.size(); i++)
@@ -27,10 +27,10 @@ int deviation( int* a, int n )
 	double stddev = 0;
 	for(size_t i = 0; i <= v.size() -1; i++)
 	{
-		double stddev = stddev + (v[i] - mean) * (v[i] - mean); 
+		stddev = stddev + (v[i] - mean) * (v[i] - mean); 
 	}
 	stddev /= v.size();
-	if( stddev = 0)
+	if( stddev == 0)
 		std::cout << "Sigma is zero." << std::endl;
 	return sqrt(stddev);
 }
