30c7ff1b76f304256619713e2c6710cb2939e87a
Brandon Kimberly <bk685616@ohio.edu>
2019-02-08 13:34:00 -0500

    Completed TODO

--- hw_broken.cc
+++ hw_broken.cc
@@ -2,9 +2,7 @@
 #include <vector>
 #include <cmath>
 
-//TODO Revert changes to function params.
-
-double deviation( std::vector<int> a, int n);
+double deviation( int* a, int n);
 
 int main()
 {
@@ -13,12 +11,13 @@ int main()
 	while( _10 >= 0 )
 	{
 		v.push_back(_10);
-		std::cout << deviation(v, v.size()) << std::endl;
+		int* front = &v.front();
+		std::cout << deviation(front, v.size()) << std::endl;
 		_10--;
 	}
 }
 
-double deviation( std::vector<int> a, int n )
+double deviation( int* a, int n )
 {
 	int sum = 0;
 	for(size_t i = 0; i < n; i++)
